688429f130b8d6fa3cd56538c236aaed8458729f
SethNdagijimana <sethreas@gmail.com>
2022-07-19 23:31:51 +0200

    updates

--- src/components/Main-Section/Card/index.jsx
+++ src/components/Main-Section/Card/index.jsx
@@ -36,7 +36,59 @@ class Card extends React.Component {
                 </div>
                 
                 {/* second card */}
+                <div className="card">
+                    <div className="top-section">
+                        <img src={this.props.cardPicture} alt="" />
+                        <div className="top-section-text">
+                            <p>{this.props.level}</p>
+                            <h4>{this.props.technology}</h4>
+                        </div>
+                    </div>
+                    <div className="middle-section">
+                        <p>by</p>
+                        
+                        <div className="profile">
+                            <img className="profile-img" src={this.props.picture} alt="" />
+                            <p>{this.props.name}</p>
+                        </div>
+                    </div>
+                    <hr className="line" />
+
+                    <div className="bottom-section">
+                        <p>{this.props.percentage}</p>
+                        <p>{this.props.ratio}</p>
+                        <img className="flag" src={this.props.flag} alt="" />
+                    </div>
                 
+                </div>
+
+                {/* thirdcard */}
+
+                <div className="card">
+                    <div className="top-section">
+                        <img src={this.props.cardPicture} alt="" />
+                        <div className="top-section-text">
+                            <p>{this.props.level}</p>
+                            <h4>{this.props.technology}</h4>
+                        </div>
+                    </div>
+                    <div className="middle-section">
+                        <p>by</p>
+                        
+                        <div className="profile">
+                            <img className="profile-img" src={this.props.picture} alt="" />
+                            <p>{this.props.name}</p>
+                        </div>
+                    </div>
+                    <hr className="line" />
+
+                    <div className="bottom-section">
+                        <p>{this.props.percentage}</p>
+                        <p>{this.props.ratio}</p>
+                        <img className="flag" src={this.props.flag} alt="" />
+                    </div>
+                
+                </div>
             </div>
         )
     }
